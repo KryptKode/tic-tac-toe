81b4f7177ac16e60433076ad5bb219698292dd1b
KryptKode <paulakhamiogu@gmail.com>
2019-06-14 21:12:29 +0100

    Added history

--- src/index.js
+++ src/index.js
@@ -78,7 +78,7 @@ class Game extends React.Component {
     }
 
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice()
         if (calculateWinner(squares) || squares[i]) {
@@ -90,6 +90,7 @@ class Game extends React.Component {
             history: history.concat([{
                 squares: squares
             }]),
+            stepNumber:history.length,
             xIsNext: !this.state.xIsNext
         });
     }
@@ -103,7 +104,7 @@ class Game extends React.Component {
 
     render() {
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
 
         const moves = history.map((step, move) => {
